6ab8cdb0555a41ac09d1d8e4a7bec4adc887fcd6
Dindin10 <141310565+Dindin10@users.noreply.github.com>
2025-09-26 11:44:43 +0800

    finish calculator functionality

--- script.js
+++ script.js
@@ -8,13 +8,63 @@ function multiply(a, b) {
     return a * b;
 }
 function divide(a, b) {
+    if(!b) return NaN;
     return a / b;
 }
 
 function operate(a, b, func) {
-    return func(a, b);
+    switch(func) {
+        case "+":
+            return add(a, b);
+        case "-":
+            return subtract(a, b);
+        case "x":
+            return multiply(a, b);
+    }
+    return divide(a, b);
 }
 
-let num1 = 0;
-let num2 = 0;
-let result = 0;
+let num1 = "";
+let num2 = "";
+let operator = "";
+
+const display = document.querySelector(".display");
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+const clear = document.querySelector("#clear");
+const equal = document.querySelector("#equal");
+
+numbers.forEach(item => item.addEventListener("click", (e) => {
+    if(operator) {
+        num2 += e.target.textContent;
+        display.textContent = num2;
+    }
+    else {
+        num1 += e.target.textContent;
+        display.textContent = num1;
+    }
+}))
+
+operators.forEach(item => item.addEventListener("click", (e) => {
+    if(num2) {
+        num1 = String(operate(+num1, +num2, operator));
+        num2 = "";
+        display.textContent = num1;
+    }
+    operator = e.target.textContent;
+}))
+
+equal.addEventListener("click", (e) => {
+    num2 ? display.textContent = operate(+num1, +num2, operator)
+        : display.textContent = num1;
+    num1 = "";
+    num2 = "";
+    operator = "";
+})
+
+clear.addEventListener("click", (e) => {
+    display.textContent = "";
+    num1 = "";
+    num2 = "";
+    operator = "";
+})
